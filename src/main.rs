9c5dae2ab5706ef73e6b6bdb8c99049e767cf79d
Guilherme Augusto de Souza <guilhermeaso@protonmail.com>
2021-06-21 04:28:34 -0300

    working input and write to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+	
 fn main() {
-    println!("Hello, world!");
+	use std::io::Write;
+	
+	let mut input = String::new();
+	let mut file = std::fs::File::create("data.txt").expect("create file");
+
+	match io::stdin().read_line(&mut input) {
+		Ok(n) => {
+			println!("{} bytes read", n);
+			println!("{}", input);
+		}
+		Err(error) => println!("error: {}", error),
+	}
+
+	file.write_all(input.as_bytes()).expect("write failed");
+	println!("Data writen to file");
 }
